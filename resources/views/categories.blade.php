b1dd39282a2dde32c716e5bb799bc11e91747422
JustqMax <maximshishov228@gmai.com>
2021-10-16 01:33:14 +0300

    d

--- resources/views/categories.blade.php
+++ resources/views/categories.blade.php
@@ -5,34 +5,18 @@
 @section('main_content')
     <div class="container">
         <div class="starter-template">
-            <div class="panel">
-                <a href="/categories/mobiles">
-                    <img src="http://internet-shop.tmweb.ru/storage/categories/mobile.jpg">
-                    <h2>Мобильные телефоны</h2>
-                </a>
+            @foreach ($categories as $category)
+                <div class="panel">
+                    <a href="{{route('category', $category->code)}}">
+                        <img src="http://internet-shop.tmweb.ru/storage/categories/mobile.jpg">
+                        <h2>{{ $category->name }}</h2>
+                    </a>
 
-                <p>
-                    В этом разделе вы найдёте самые популярные мобильные телефонамы по отличным ценам!
-                </p>
-            </div>
-            <div class="panel">
-                <a href="/categories/portable">
-                    <img src="http://internet-shop.tmweb.ru/storage/categories/portable.jpg">
-                    <h2>Портативная техника</h2>
-                </a>
-                <p>
-                    Раздел с портативной техникой.
-                </p>
-            </div>
-            <div class="panel">
-                <a href="/categories/appliances">
-                    <img src="http://internet-shop.tmweb.ru/storage/categories/appliance.jpg">
-                    <h2>Бытовая техника</h2>
-                </a>
-                <p>
-                    Раздел с бытовой техникой
-                </p>
-            </div>
+                    <p>
+                        {{ $category->description }}
+                    </p>
+                </div>
+            @endforeach
         </div>
     </div>
 @endsection
